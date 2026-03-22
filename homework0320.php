926935f665f32484c1bb8bef159454874410c233
a1111428 <a1111428@mail.nuk.edu.tw>
2026-03-22 19:19:14 +0800

    Create homework0320.php

---
+++ homework0320.php
@@ -0,0 +1,89 @@
+<html>
+<head>    
+    
+    <title>2026 高雄大學夏令營</title>
+
+</head>
+<body>
+
+    <CENTER>
+        <h1>2026 高雄大學程式夏令營</h1>
+        <font size=4 color=green>充滿挑戰與樂趣的暑假體驗！</font>
+    </CENTER>
+
+    <hr>
+    
+    <h2>活動簡介</h2>
+
+
+    <p>
+    歡迎來到高雄大學！本次夏令營專為對程式設計與人工智慧有興趣的青少年設計。我們將帶領學員體驗高雄大學的校園氛圍，並學習前沿的科技知識。
+    <p>
+    本活動將由高大優秀的教授與助教團隊授課，搭配在校學長姐的教學，讓您在輕鬆的氣氛中掌握基礎技能。
+    <p>
+    <font size=5 color=red><b>名額有限，趕快報名!</font>
+
+    <hr>
+
+    <h2>活動特色</h2>
+
+
+    <ul>
+        <li>高雄大學校園巡禮：參觀圖資大樓及管理學院
+        <li>Python 基礎與人工智慧入門
+        <li>物聯網 (IoT) 實作體驗
+    </ul>
+
+    <hr>
+
+    <h2>行程表</h2>
+
+
+    <TABLE BORDER="1">
+        <TR>
+            <TH>時間</TH>
+            <TH>活動主題</TH>
+            <TH>地點</TH>
+        </TR>
+        <TR>
+            <TD>上午10:00~12:00</TD>
+            <TD>智慧科技講座</TD>
+            <TD>圖資演講廳</TD>
+        </TR>
+        <TR>
+            <TD>中午12:00~13:30</TD>
+            <TD>享用高大學餐</TD>
+            <TD>學生第二宿舍</TD>
+        </TR>
+        <TR>
+            <TD>下午14:00~17:00</TD>
+            <TD>上機實作 (AI)</TD>
+            <TD>圖資107、108教室</TD>
+        </TR>
+    </TABLE>
+    
+    <hr>
+
+    <h2>報名流程</h2>
+
+
+    <ol>
+        <li>填寫高雄大學官方夏令營報名表單
+        <li>完成活動費用繳納
+        <li>確認收到主辦單位發送的確認郵件
+    </ol>
+
+    <hr>
+
+    <h2>聯絡我們</h2>
+
+
+    如有任何疑問，請透過以下方式聯絡我們：<br>
+    <A HREF="http://www.nuk.edu.tw">高雄大學 (NUK) 官方網站</A><br>
+    撥打電話至課程負責人(阿銓):0912123456<br>
+    <A HREF="http://www.nuk.edu.tw">連結到夏令營報名專頁</A><br>
+
+    <IMG SRC="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi684QC1LVm0cFB8PicYAUI_oF6PuxpN6Ktw&s" ALT="高雄大學夏令營活動照片"></IMG>
+    
+</body>
+</html>
