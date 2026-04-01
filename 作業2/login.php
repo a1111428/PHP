6e2edcca41e29139d4ac09b02835611d52be0035
a1111428 <a1111428@mail.nuk.edu.tw>
2026-04-01 18:43:26 +0800

    Delete 作業2 directory

--- 作業2/login.php
+++
@@ -1,9 +0,0 @@
-<form action="logincheck.php" method="POST">
-    <title>高雄大學夏令營登入介面</title>
-
-
-    帳號:<input type ="text" name = "input_ID"><br>
-    密碼:<input type ="password" name = "input_PWD"><br>
-
-    <input type ="submit" value="登入">
-</form>
