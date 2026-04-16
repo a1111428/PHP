9eb867a05da120ccc66331507dc82b4772e1374a
a1111428 <a1111428@mail.nuk.edu.tw>
2026-04-16 23:05:26 +0800

    Create teacher_page.php

---
+++ a1111428_許祐銓_作業3/A/teacher_page.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+if($_SESSION['login']=='teacher'){
+    echo "<h1>Welcome! {$_COOKIE['uName']}</h1><br>";
+    echo"<a href='logout1.php'>logout</a>";
+    }else{
+    echo"<h1>非法進入網頁</h1>";
+    header("Refresh:1;url=login1.php");
+}
+
+?>
